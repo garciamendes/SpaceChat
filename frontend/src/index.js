eb980bb025115163de646fbaf5bb7ef3b4b6ca11
garciamendes <garciaprog0101@gmail.com>
2021-03-06 17:32:30 -0300

    Project base

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+// React
+import React from 'react'
+
+// Third party
+import ReactDOM from 'react-dom'
+
+// Loca
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
