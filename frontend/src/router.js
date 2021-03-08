6c512f0bfe34cba554487aab9483f0dd836c5418
garciamendes <garciaprog0101@gmail.com>
2021-03-08 15:00:44 -0300

    adjuste to router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -12,7 +12,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Home}/>
+        <Route path='/' component={Home}/>
       </Switch>
     </BrowserRouter>
   )
